63528b2b9a4f72ec4683ed36277488b47ce2092d
yuta.sugimine <yu0210ta.sug@gmail.com>
2022-09-20 18:10:36 +0900

    マルチログイン不具合対応View側

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,11 +15,11 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-            @if(auth('admin')->user())
+            @if(request()->is('admin*'))
                 @include('layouts.admin-navigation')
-            @elseif(auth('companies')->user())
+            @elseif(request()->is('company*'))
                 @include('layouts.company-navigation')
-            @elseif(auth('users')->user())
+            @else
                 @include('layouts.user-navigation')
             @endif
 
